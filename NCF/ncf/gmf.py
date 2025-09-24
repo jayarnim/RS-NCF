b2b6028bf8e6cb0ceffd8cfaa95a592bc13cb830
jayarnim <jayarnim727@gmail.com>
2025-09-24 22:50:03 +0900

    commit

--- NCF/ncf/gmf.py
+++ NCF/ncf/gmf.py
@@ -74,6 +74,9 @@ class Module(nn.Module):
         )
         self.item_embed = nn.Embedding(**kwargs)
 
+        nn.init.normal_(self.user_embed.weight, mean=0.0, std=0.01)
+        nn.init.normal_(self.item_embed.weight, mean=0.0, std=0.01)
+
         kwargs = dict(
             in_features=self.n_factors,
             out_features=1,
