49cbb9b34d7f4713f6acffc7a00cdfdcce80cb87
jayarnim <jayarnim727@gmail.com>
2025-10-07 14:12:20 +0900

    check climf error

--- LOOP/loss_fn/listwise.py
+++ LOOP/loss_fn/listwise.py
@@ -1,7 +1,8 @@
-import torch
+import torch.nn.functional as F
+
 
 def climf(pos, neg):
     diff = neg - pos.unsqueeze(1)
-    max_pos_term  = torch.log(torch.sigmoid(pos) + 1e-10)
-    min_diff_term = torch.log(1 - torch.sigmoid(diff) + 1e-10).sum(dim=1)
-    return -(max_pos_term + min_diff_term)
+    max_pos_term  = F.logsigmoid(pos)
+    min_diff_term = F.logsigmoid(-diff).sum(dim=1)
+    return -(max_pos_term + min_diff_term).mean()
